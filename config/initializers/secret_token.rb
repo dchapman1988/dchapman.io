2748d7487f47e55b9a85a1d093d2c535606fb1ea
David Chapman <david@isotope11.com>
2013-07-18 09:11:00 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DchapmanIo::Application.config.secret_key_base = 'b6ea3eccde22e68f09727a331258b0dc3a38dbe84d61a4636c7e6c45508cad00050cc707c9326caea6c60baa71788659a3e313aea477113cf21079da73b2dbbf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DchapmanIo::Application.config.session_store :cookie_store, key: '_dchapman_io_session'
