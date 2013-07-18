fd216926b20fa7c3d3b1cce855962e95b2bcd5f3
David Chapman <david@isotope11.com>
2013-07-18 14:12:13 -0500

    heroku sucks with assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ DchapmanIo::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -56,6 +56,8 @@ DchapmanIo::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
+
+  config.assets.initialize_on_precompile = true
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
