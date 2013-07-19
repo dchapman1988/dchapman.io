88d4493f4c19c42f5d4069d9a9ba99ff5d882c7b
David Chapman <david@isotope11.com>
2013-07-19 14:51:34 -0500

    Fixes order of posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   extend ::FriendlyId
   friendly_id :title, use: :slugged
 
+  default_scope order('created_at DESC')
+
   validates :title, uniqueness: true
   validates :body, presence: true
 end
