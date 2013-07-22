037352c6b116c59aeb794009d970c0d2df077d4e
David Chapman <david@isotope11.com>
2013-07-22 14:25:30 -0500

    Add disqus

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def page_load_time
     "Page Rendered in #{sprintf('%.3f', (Time.now.usec - @start_time).abs / 1000000.0)} seconds."
   end
+
+  def current_url
+    url_for(:only_path => false)
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  it 'should have a page_load_time_helper' do
+  it 'should have a page_load_time helper' do
     @start_time = Time.now.usec
     expect(helper.page_load_time).to be_kind_of String
+  end
+
+  it 'should have a current_url helper' do
+    helper.should respond_to :current_url
   end
 end
