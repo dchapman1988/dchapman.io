92e8499dff45217fa924b9ba1ba080e89d52743f
David Chapman <david@isotope11.com>
2013-07-22 11:08:08 -0500

    Don't fetch bg from external site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def page_load_time_helper
+  def page_load_time
     "Page Rendered in #{sprintf('%.3f', (Time.now.usec - @start_time).abs / 1000000.0)} seconds."
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe ApplicationHelper do
   it 'should have a page_load_time_helper' do
     @start_time = Time.now.usec
-    expect(helper.page_load_time_helper).to be_kind_of String
+    expect(helper.page_load_time).to be_kind_of String
   end
 end
